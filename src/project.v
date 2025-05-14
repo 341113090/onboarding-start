dda74d7e03f5aa950f58b96765927576f113c69a
341113090 <nathanthian@outlook.com>
2025-05-13 22:00:51 -0400

    Fixing dumb stuff

--- src/project.v
+++ src/project.v
@@ -1,8 +1,24 @@
-// Change the module name!
-module tt_um_uwasic_onboarding_nathan_thian(...)
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+module tt_um_uwasic_onboarding_nathan_thian(
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
+);
 
   // Add this inside the module block
   assign uio_oe = 8'hFF; // Set all IOs to output
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
   
   // Create wires to refer to the values of the registers
   wire [7:0] en_reg_out_7_0;
@@ -22,6 +38,18 @@ module tt_um_uwasic_onboarding_nathan_thian(...)
     .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out})
   );
+
+  spi_peripheral spi_peripheral_inst(
+    .clk(clk),
+    .rst_n(rst_n),
+    .ui_in(ui_in),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+  )
+
   // Add uio_in and ui_in[7:3] to the list of unused signals:
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 endmodule
--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -3,7 +3,7 @@
 module spi_peripheral (
     input  wire       clk,      // clock
     input  wire       rst_n,     // reset_n - low to reset
-    input wire [2:0] ui_in // nCS [2] COPI [1] SCLK [0]
+    input wire [7:0] ui_in // nCS [2] COPI [1] SCLK [0]
     output reg [7:0] en_reg_out_7_0,
     output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
